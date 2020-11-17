ac94c63c0d6523ac9807e9b4ca8ce55e9493185f
taoyawei@weihaizixun.com <taoyawei@weihaizixun.com>
2020-11-18 07:50:52 +0800

    编写注册页面逻辑

--- client/src/view/user/modify.jsx
+++ client/src/view/user/modify.jsx
@@ -7,6 +7,7 @@ import Shar from './shar.jsx'
 import './scss/modify.scss'
 import Icon from '../component/icon/icon.js'
 import {
+  List,
   InputItem,
   Button
 } from 'antd-mobile'
@@ -14,29 +15,57 @@ class Modify extends Component {
   render(){
     return (
       <div className="register">
-        <div className="register-header">欢迎登录</div>
+        <div className="register-header">修改密码</div>
         <div className="img-box">
           <img src={require('./image/head.jpg').default} alt="头像"  className="regitter-img" />
         </div>
         <div className="register-content">
-          <div className="content-item">
-            <InputItem className="register-mobile" placeholder="请输入手机号" maxLength={11} />
-            <span className="mobile-icon">
-              <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconshouji" />
-            </span>
-          </div>
-          <div className="content-item">
-            <InputItem className="register-mobile" placeholder="请输入老密码" maxLength={6} />
-            <span className="mobile-icon">
-              <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconmima" />
-            </span>
-          </div>
-          <div className="content-item">
-            <InputItem className="register-mobile" placeholder="请输入新密码" maxLength={6} />
-            <span className="mobile-icon">
-              <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconmima1" />
-            </span>
-          </div>
+        <List>
+            <List.Item>
+              <div className="content-item">
+                <InputItem
+                  clear
+                  type="number"
+                  className="register-mobile"
+                  value={this.state.mobile}
+                  placeholder="请输入手机号"
+                  maxLength={11}
+                  />
+                <span className="mobile-icon">
+                  <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconshouji" />
+                </span>
+              </div>
+            </List.Item>
+            <List.Item>
+              <div className="content-item">
+                <InputItem
+                  clear
+                  className="register-mobile"
+                  value={this.state.nickName}
+                  placeholder="老密码"
+                  maxLength={32}
+                />
+                <span className="mobile-icon">
+                  <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconyonghu" />
+                </span>
+              </div>
+            </List.Item>
+            <List.Item>
+              <div className="content-item">
+                <InputItem
+                  clear
+                  className="register-mobile"
+                  value={this.state.password}
+                  placeholder="请设置新密码"
+                  maxLength={6}
+                  type="password"
+                />
+                <span className="mobile-icon">
+                  <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconmima" />
+                </span>
+              </div>
+            </List.Item>
+          </List>
         </div>
         <div className="modify-btn">
           <Button className="register-button">修改密码</Button>
--- client/src/view/user/register.jsx
+++ client/src/view/user/register.jsx
@@ -7,10 +7,50 @@ import Shar from './shar.jsx'
 import './scss/register.scss'
 import Icon from '../component/icon/icon.js'
 import {
+  List,
   InputItem,
   Button
 } from 'antd-mobile'
 class Register extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mobile: null,
+      nickName: null,
+      password: null
+    }
+    // this.doSetData = this.doSetData.bind(this)
+  }
+  doSetData(msg, e) {
+    // this.setState({
+    //   mobile: e
+    // })
+    switch(msg) {
+      case 'mobile':
+        this.setState({
+          mobile: e
+        })
+        break
+      case 'nickName':
+        this.setState({
+          nickName: e
+        })
+        break
+      case 'password':
+        this.setState({
+          password: e
+        })
+        break
+      default:
+        console.log('错了')
+        break
+    }
+  }
+  doRegister () {
+    console.log(this.state.mobile)
+    console.log(this.state.nickName)
+    console.log(this.state.password)
+  }
   render(){
     return (
       <div className="register">
@@ -19,27 +59,58 @@ class Register extends Component {
           <img src={require('./image/head.jpg').default} alt="头像"  className="regitter-img" />
         </div>
         <div className="register-content">
-          <div className="content-item">
-            <InputItem className="register-mobile" placeholder="请输入手机号" maxLength={11} />
-            <span className="mobile-icon">
-              <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconshouji" />
-            </span>
-          </div>
-          <div className="content-item">
-            <InputItem className="register-mobile" placeholder="请设置昵称" maxLength={32} />
-            <span className="mobile-icon">
-              <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconyonghu" />
-            </span>
-          </div>
-          <div className="content-item">
-            <InputItem className="register-mobile" placeholder="请设置密码" maxLength={6} />
-            <span className="mobile-icon">
-              <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconmima" />
-            </span>
-          </div>
+          <List>
+            <List.Item>
+              <div className="content-item">
+                <InputItem
+                  clear
+                  type="number"
+                  className="register-mobile"
+                  value={this.state.mobile}
+                  placeholder="请输入手机号"
+                  maxLength={11}
+                  onChange={this.doSetData.bind(this, 'mobile')}
+                  />
+                <span className="mobile-icon">
+                  <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconshouji" />
+                </span>
+              </div>
+            </List.Item>
+            <List.Item>
+              <div className="content-item">
+                <InputItem
+                  clear
+                  className="register-mobile"
+                  value={this.state.nickName}
+                  placeholder="请设置昵称"
+                  maxLength={32}
+                  onChange={this.doSetData.bind(this, 'nickName')}
+                />
+                <span className="mobile-icon">
+                  <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconyonghu" />
+                </span>
+              </div>
+            </List.Item>
+            <List.Item>
+              <div className="content-item">
+                <InputItem
+                  clear
+                  className="register-mobile"
+                  value={this.state.password}
+                  placeholder="请设置密码"
+                  maxLength={6}
+                  type="password"
+                  onChange={this.doSetData.bind(this, 'password')}
+                />
+                <span className="mobile-icon">
+                  <Icon style={{width: '20px', height: '20px', color: '#ffffff'}} type="iconmima" />
+                </span>
+              </div>
+            </List.Item>
+          </List>
         </div>
         <div className="register-btn">
-          <Button className="register-button">注册</Button>
+          <Button className="register-button" onClick={this.doRegister.bind(this)}>注册</Button>
         </div>
       </div>
     )
