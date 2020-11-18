0e947df5f499b3a3695030f38beb66220b829eef
taoyawei@weihaizixun.com <taoyawei@weihaizixun.com>
2020-11-18 13:41:34 +0800

    添加评论接口

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -8,6 +8,7 @@ const {
   addComment
 } = require('../controller/comments.js')
 
+// 添加评论
 router.post('/add', loginCheck, async (ctx, next) => {
   const {content, create_time, user_id, book_id} = ctx.request.body
   ctx.body = await addComment({
